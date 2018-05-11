561d0482bb80af698494e38209b5a4230449f266
Gaetan Semet <gaetan@xeberon.net>
2018-05-11 23:06:19 +0200

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cd $(dirname $0)
-export TMPDIR=$(mkdir -p tmp && cd tmp && pwd)
-echo "Setting tmpdir to: $TMPDIR)"
-echo "Installing/updating home assistant"
-if [[ ! -d .venv ]]; then
-	PIPENV_VENV_IN_PROJECT=1 pipenv install --python /usr/local/bin/python3.5 hass
-else
-	PIPENV_VENV_IN_PROJECT=1 pipenv update
-fi
+export PATH=$HOME/.local/bin:$PATH
+
+cd $(dirname $0 && pwd)
+export TMPDIR=$(cd tmp && pwd)
+
+pipenv update
