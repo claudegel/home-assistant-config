1c4c34377daa0780dda518a64b9a5cc84a47c65d
Gaetan Semet <gaetan@xeberon.net>
2018-08-17 22:51:13 +0200

    update to 0.76

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ./stop-daemon.sh
+cd $(dirname $0 && pwd)
 
 export PATH=$HOME/.local/bin:$PATH
-
-cd $(dirname $0 && pwd)
 export TMPDIR=$(cd tmp && pwd)
 export PIP_NO_CACHE_DIR=off
 export PIPENV_CACHE_DIR=$TMPDIR/pip
+
 pipenv update
 
 ./restart-daemon.sh
