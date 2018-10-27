2a6f06b5a01296af2249f9bf3a2afa4d66f83303
Gaetan Semet <gaetan@xeberon.net>
2018-10-27 11:51:43 +0200

    add surv card in lovelace, enable remote in bathroom heater

--- install-hass.sh
+++ install-hass.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y libudev-dev
+sudo apt-get install -y \
+    ffmpeg
+    libudev-dev \
+    nmap \
+    raspberrypi-kernel-headers \
 
 mkdir -p .venv tmp
 pipenv install --skip-lock
 pipenv run pip install custom_components/climate/pid_controller/
-
