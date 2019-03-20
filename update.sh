b87bdeb1da8881186cbc7c39b00d817c8d5793c9
Gaetan Semet <gaetan@xeberon.net>
2019-03-20 23:01:08 +0100

    update ui and freze version in update.sh

--- update.sh
+++ update.sh
@@ -3,14 +3,13 @@
 cd $(dirname $0)
 
 export PATH=$HOME/.local/bin:$PATH
-#export TMPDIR=$(cd tmp && pwd)
-#export PIP_NO_CACHE_DIR=off
-#export PIPENV_CACHE_DIR=$TMPDIR/pip
 
 source .venv/bin/activate
 
+./stop-daemon.sh
+
 pip install --upgrade pip pipenv wheel
-pip install --upgrade homeassistant
+pip install --upgrade homeassistant==$(cat .homeassistant.version)
 
 ./restart-daemon.sh
 
