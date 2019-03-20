eb498a6b367a2b6e0110bb3b1e5ecfbc1fd37b27
Gaetan Semet <gaetan@xeberon.net>
2019-03-20 22:15:50 +0100

    trying install appdaemon

--- install-daemons.sh
+++ install-daemons.sh
@@ -2,9 +2,11 @@
 
 sed "s#<path to homeassistant configuration directory>#$PWD#g" home-assistant@pi.service.in > /etc/systemd/system/home-assistant@pi.service
 sed "s#<path to homeassistant configuration directory>#$PWD#g" home-assistant-configurator@pi.service.in > /etc/systemd/system/home-assistant-configurator@pi.service
+sed "s#<path to homeassistant configuration directory>#$PWD#g" home-assistant-appdaemon@pi.service.in > /etc/systemd/system/home-assistant-appdaemon@pi.service
 
 echo "Please edit path and user"
 
 sudo systemctl enable home-assistant@pi.service
+sudo systemctl enable home-assistant-appdaemon@pi.service
 sudo systemctl enable home-assistant-configurator@pi.service
 
