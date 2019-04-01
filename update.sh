4259de87ad5c13d9f9978ea97efa7fe286c4ad39
Gaetan Semet <gaetan@xeberon.net>
2019-04-01 23:43:10 +0200

    update and add multisensor

--- update.sh
+++ update.sh
@@ -6,8 +6,6 @@ export PATH=$HOME/.local/bin:$PATH
 
 source .venv/bin/activate
 
-./stop-daemon.sh
-
 pip install --upgrade pip pipenv wheel
 pip install --upgrade homeassistant==$(cat .homeassistant.version)
 
