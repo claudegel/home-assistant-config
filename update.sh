34460d3566e3109dc14189d7ab0e831da6e0797e
Gaetan Semet <gaetan@xeberon.net>
2019-05-19 23:16:18 +0200

    hass 0.93.1

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ export PATH=$HOME/.local/bin:$PATH
 source .venv/bin/activate
 
 pip install --upgrade pip pipenv wheel
-pip install --upgrade pymysql mysqlclient
+pip install --upgrade pymysql mysqlclient bs4
 pip install --upgrade homeassistant==$(cat .homeassistant.version)
 
 ./restart-daemon.sh
